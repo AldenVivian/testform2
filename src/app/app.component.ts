a4961beb406fbcfdb8b3c9cf3a7a438c5c1aa9d5
AldenVivian <aldenvivianchamp@gmail.com>
2021-06-23 17:13:51 +0530

    POSTchanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'testform2';
+  
+  private count:number = 1;
+
+phoneNumberIds:number[] = [1];
+  postId: any;
+
+constructor(private http: HttpClient){}
+
+
+remove(i:number) {
+  this.phoneNumberIds.splice(i, 1);
+}
+
+add() {
+  this.phoneNumberIds.push(++this.count);
+}
+
+
+
+register (ngForm: NgForm) {
+  console.log('Successful registration');
+  //console.log(ngForm);
+  var body = ngForm.value;
+  console.log(body);
+ 
+ this.http.post<any>('http://localhost:4200/', body).subscribe(data => {})
+
+ 
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
